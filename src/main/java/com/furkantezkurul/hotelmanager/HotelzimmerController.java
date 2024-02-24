5bdd936fc9661b66911b35a4cf370c683de87f7c
Furkan Tezkurul <furkantezkurul@gmail.com>
2024-02-24 15:28:50 +0100

    Add search by id

--- src/main/java/com/furkantezkurul/hotelmanager/HotelzimmerController.java
+++ src/main/java/com/furkantezkurul/hotelmanager/HotelzimmerController.java
@@ -1,13 +1,16 @@
 package com.furkantezkurul.hotelmanager;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/hotelzimmer")
@@ -18,5 +21,10 @@ public class HotelzimmerController {
     @GetMapping
     public ResponseEntity<List<Hotelzimmer>> getAllHotelzimmer(){
       return new ResponseEntity<List<Hotelzimmer>>(hotelzimmerService.allHotelzimmer(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Hotelzimmer>> getSingleHotelzimmer(@PathVariable ObjectId id){
+        return new ResponseEntity<Optional<Hotelzimmer>>(hotelzimmerService.singleHotelzimmer(id), HttpStatus.OK);
     }
 }
--- src/main/java/com/furkantezkurul/hotelmanager/HotelzimmerService.java
+++ src/main/java/com/furkantezkurul/hotelmanager/HotelzimmerService.java
@@ -1,9 +1,11 @@
 package com.furkantezkurul.hotelmanager;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class HotelzimmerService {
@@ -12,5 +14,9 @@ public class HotelzimmerService {
     public List<Hotelzimmer> allHotelzimmer(){
         System.out.println(hotelzimmerRepository.findAll());
         return hotelzimmerRepository.findAll();
+    }
+
+    public Optional<Hotelzimmer> singleHotelzimmer(ObjectId id){
+        return hotelzimmerRepository.findById(id);
     }
 }
